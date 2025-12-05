ba320967a0ce5ea11fe229a82aac468db30e9ab1
Viktoriia Tsukan <vika.tsukan@gmail.com>
2025-12-05 22:42:07 +0200

    chore: Migrated to Angular Signals

--- frontend/src/app/features/components/image-processing/image-processing.component.ts
+++ frontend/src/app/features/components/image-processing/image-processing.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { Observable, take } from 'rxjs';
+import { take } from 'rxjs';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 import { ProcessStep } from '../../../store/image/image-state.model';
@@ -19,16 +20,18 @@ import { ImageStore } from '../../../store/image/image-store';
     RouterModule
   ],
   templateUrl: './image-processing.component.html',
-  styleUrl: './image-processing.component.scss'
+  styleUrl: './image-processing.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImageProcessingComponent implements OnInit {
   ProcessStepEnum = ProcessStep;
-  currentStep$!: Observable<ProcessStep>;
+  currentStep: Signal<ProcessStep>;
 
-  constructor(private imageStore: ImageStore, private router: Router) { }
+  constructor(private imageStore: ImageStore, private router: Router) {
+    this.currentStep = toSignal(this.imageStore.processStep$, { initialValue: ProcessStep.Initial });
+  }
 
   ngOnInit(): void {
-    this.currentStep$ = this.imageStore.processStep$;
     this.processImage();
   }
 
